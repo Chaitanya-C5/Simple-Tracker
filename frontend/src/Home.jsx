ca45d3f47719db5f651e59902ea556dcc595fcf5
Chaitanya-C5 <gellachaitanyavenkatasai@gmail.com>
2024-12-09 10:13:14 +0530

    Change Code

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,69 +1,71 @@
-import io from 'socket.io-client'
-import { useEffect, useState } from 'react'
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Home() {
-  const [error, setError] = useState(null)
-  const [position, setPosition] = useState(null)
+  const [error, setError] = useState(null);
+  const [position, setPosition] = useState(null);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    const socket = io.connect('https://simple-tracker-backend.onrender.com')
-    if(navigator.geolocation) {
-        navigator.geolocation.watchPosition(
-            (position) => {
-                const { latitude, longitude } = position.coords
-                setPosition([latitude, longitude])
-                socket.emit('send-location', { latitude, longitude })
-            },
-            (error) => {
-                console.log(error)
-                setError(error)
-            },
-            {
-                enableHighAccuracy: true,
-                maximumAge: 0, // No caching
-                timeout: 3000,
-            }
-        )
+    const socket = io.connect("https://simple-tracker-backend.onrender.com");
+
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setPosition([latitude, longitude]);
+          socket.emit("send-location", { latitude, longitude });
+        },
+        (err) => {
+          console.log(err);
+          setError(err.message); 
+        },
+        {
+          enableHighAccuracy: true,
+          maximumAge: 0, // No caching
+          timeout: 10000, 
+        }
+      );
     }
 
-    socket.on('receive-location', (data) => {
-        setUsers((prevUsers) => {
-            const newUsers = prevUsers.filter((user) => user.id !== data.id);
-            newUsers.push({ id: data.id, location: [data.latitude, data.longitude] });
-            return newUsers;
-          });
-    })
-    
-    socket.on('disconnect-user', ({id}) => {
-        setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id))
+    socket.on("receive-location", (data) => {
+      setUsers((prevUsers) => {
+        const newUsers = prevUsers.filter((user) => user.id !== data.id);
+        newUsers.push({ id: data.id, location: [data.latitude, data.longitude] });
+        return newUsers;
+      });
     });
-        
-  },[])
 
-  if(error) return <div>{error}</div>
+    socket.on("disconnect-user", ({ id }) => {
+      setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id));
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  if (error) return <div>Error: {error}</div>;
   if (position === null) return <div>Loading...</div>;
 
   return (
-    <MapContainer center={position} zoom={15} style={{ height: "100%", width: "100%" }}> 
-        <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
+    <MapContainer center={position} zoom={15} style={{ height: "100%", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
 
-        {
-            users.length > 0 && users.map((user) => 
-                user.location ? (
-                    <Marker key={user.id} position={user.location}>
-                            <Popup>Another user is here!</Popup>
-                    </Marker> 
-                        
-                ) : null
-            )
-        }
+      {users.length > 0 &&
+        users.map((user) =>
+          user.location ? (
+            <Marker key={user.id} position={user.location}>
+              <Popup>User Location</Popup>
+            </Marker>
+          ) : null
+        )}
     </MapContainer>
-  )
+  );
 }
 
-export default Home
+export default Home;
